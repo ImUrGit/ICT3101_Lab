0b7dd765f53ab91dd0330fa4d3765feaba443f5a
ImUrGit <alainpierreparonda@gmail.com>
2024-10-01 16:27:51 +0800

    Last testttt

--- ICT3101_Calculator.UnitTests/AdditionalCalculatorTests.cs
+++ ICT3101_Calculator.UnitTests/AdditionalCalculatorTests.cs
@@ -19,41 +19,33 @@ public class AdditionalCalculatorTests
     [Test]
     public void GenMagicNum_WithValidInput_ReturnsCorrectMagicNumber()
     {
-        // Act
         double result = _calculator.GenMagicNum(1, _mockFileReader.Object);
-
-        // Assert
+        
         Assert.That(result, Is.EqualTo(84)); // Mock value 42 doubled is 84
     }
 
     [Test]
     public void GenMagicNum_WithOutOfBoundsInput_ReturnsZero()
     {
-        // Act
         double result = _calculator.GenMagicNum(10, _mockFileReader.Object); // Out of bounds
-
-        // Assert
+        
         Assert.That(result, Is.EqualTo(0)); // Expect 0 since index is out of bounds
     }
 
     [Test]
     public void GenMagicNum_FileMissing_ThrowsFileNotFoundException()
     {
-        // Arrange
         _mockFileReader.Setup(fr => fr.Read(It.IsAny<string>())).Throws<FileNotFoundException>();
-
-        // Act & Assert
+        
         Assert.Throws<FileNotFoundException>(() => _calculator.GenMagicNum(1, _mockFileReader.Object));
     }
 
     [Test]
     public void GenMagicNum_WithInvalidContent_ThrowsFormatException()
     {
-        // Arrange
         _mockFileReader.Setup(fr => fr.Read("MagicNumbers.txt"))
             .Returns(new string[] { "42", "invalid" }); // Mock invalid content
-
-        // Act & Assert
+        
         Assert.Throws<FormatException>(() => _calculator.GenMagicNum(1, _mockFileReader.Object));
     }
 }
